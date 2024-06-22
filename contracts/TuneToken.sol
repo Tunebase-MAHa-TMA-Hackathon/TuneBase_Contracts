28cdf6856f7681c08c48b5299c6b91412c8bc035
Cosmodude <vladislav.lenskii@gmail.com>
2024-06-22 14:13:45 +0900

    token contract

---
+++ contracts/TuneToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title Contract of the ETHSeoul2024 Hackathon
+ * @author @cosmodude
+ * @notice ERC20 token 
+ */
+contract TuneToken is ERC20, Ownable, ERC20Permit{
+    constructor(address _owner) ERC20("TuneToken", "TUNE") Ownable(_owner) ERC20Permit("TuneToken"){}
+    
+    // set decimals to 2
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
+    }
+
+    /**
+     * @notice Mints tokens to the specified address
+     * @param to The address of the recipient
+     * @param amount Amount of tokens (with 2 decimals)
+     */
+    function mint(address to, uint256 amount) external  {
+        _mint(to, amount);
+    }
+}
