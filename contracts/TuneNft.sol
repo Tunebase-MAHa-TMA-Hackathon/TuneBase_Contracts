b66a83e2e11b7f1ac7e2fc1acc7a51dd876b6c07
Cosmodude <vladislav.lenskii@gmail.com>
2024-06-22 13:43:05 +0900

    nft contract

---
+++ contracts/TuneNft.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title Contract of the ETHSeoul2024 Hackathon
+ * @author @cosmodude
+ * @notice ERC20 token 
+ */
+contract TuneNft is ERC721, ERC721Burnable, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor(address initialOwner)
+        ERC721("TuneNft", "TUNE")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to) public {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+}
